c76ddea138bce4f547243c24a3d30418974ba881
Jeven <55917799+JevenXing@users.noreply.github.com>
2020-04-07 16:05:14 +0800

    保存

--- C_Primer_Plus/text7_ifcase/text_7.c
+++ C_Primer_Plus/text7_ifcase/text_7.c
@@ -1,8 +1,8 @@
 /*
- * @Description    :
+ * @Description    :第七章习题
  * @Date           : 2020-04-05 22:13:11
  * @LastEditors    : JEVEN
- * @LastEditTime   : 2020-04-06 11:41:36
+ * @LastEditTime   : 2020-04-07 16:03:28
  * @FilePath       : \C_2020\C_Primer_Plus\text7_ifcase\text_7.c
  */
 
@@ -21,14 +21,80 @@ void text_2();
 void text_1();
 void text_3();
 void text_4();
+void text_6();
+void text_7();
 
 int main() {
     // text_1();
     // text_2();
     // text_3();
     // text_4();
+    // text_6();
+    text_7();
+
     stop();
     return 0;
+}
+/**
+ * @description: 编写一个程序，提示用户输入一周工作的小时数，然后打印工资总
+额、税金和净收入。做如下假设：
+a.基本工资 = 1000美元/小时
+b.加班（超过40小时） = 1.5倍的时间
+c.税率： 前300美元为15%
+续150美元为20%
+余下的为25%
+用#define定义符号常量。不用在意是否符合当前的税法。
+ * @param {type}
+ * @return:
+ */
+void text_7() {
+    const double taxRate_1 = 0.15, taxRate_2 = 0.2, taxRate_3 = 0.25;
+    double h;         //工作时长
+    double incomeh;   //每小时工资
+    double total_num; //税前收入
+    double tax_num;   //税额
+    double income;    //税后收入
+    // int result;
+    printf("输入你的工作时长:");
+    while (scanf("%f", &h) >= 1) {
+        (h > 40) ? (h += (h - 40) * 1.5) : h;
+        total_num = incomeh * h;
+        if (total_num <= 300) {
+            income -= (tax_num = (total_num * taxRate_1));
+            //不到450 续150的情况 不交税
+        } else if (total_num >= (300 + 150)) {
+            //前300税额
+            tax_num = (total_num * taxRate_1);
+            //续150税额
+            tax_num += 150 * taxRate_2;
+            //剩余部分
+            tax_num += (incomeh - 540) * taxRate_3;
+            //扣税
+            income -= tax_num;
+        }
+    }
+    printf("税前工资:%.2lf,税额:%.2lf,税后工资:%.2lf", total_num, tax_num,
+           income);
+}
+/**
+ * @description: 编写程序读取输入，读到#停止，报告ei出现的次数。
+注意 该程序要记录前一个字符和当前字符。用“Receive your eieio award”这
+样的输入来测试。
+ * @param {type}
+ * @return:
+ */
+void text_6() {
+    char ch;
+    char chup = 'a';
+    int ei_count;
+    printf("输入一段字符:");
+    while ((ch = getchar()) != '#') {
+        if (chup == 'e' && ch == 'i') {
+            ei_count++;
+        }
+        chup = ch;
+    }
+    printf("ei出现了%d次.", ei_count);
 }
 /**
  * @description: 使用if else语句编写一个程序读取输入，读到#停止。用感叹号替换句
