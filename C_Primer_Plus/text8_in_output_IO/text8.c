cde787a3f6bd46abb40c3cdd516ec38f23cac612
JevenXing <879826973@qq.com>
2020-06-03 22:54:17 +0800

    1

--- C_Primer_Plus/text8_in_output_IO/text8.c
+++ C_Primer_Plus/text8_in_output_IO/text8.c
@@ -2,8 +2,8 @@
  * @Description    :
  * @Date           : 2020-06-02 16:39:06
  * @LastEditors    : JEVEN
- * @LastEditTime   : 2020-06-03 15:12:39
- * @FilePath       : \C_2020\C_Primer_Plus\text8_in_output\text8.c
+ * @LastEditTime   : 2020-06-03 22:24:15
+ * @FilePath       : \C_2020\C_Primer_Plus\text8_in_output_IO\text8.c
  */
 
 #include <ctype.h>   //字符分析函数
@@ -17,17 +17,29 @@ void stop() {
     getchar();
 }
 void Log() { printf("log:___________program start!\n"); }
+
 void echo() { //输入验证练习
-    char ch; 
+    char ch;
     while ((ch = getchar()) != '#') { //复习:运算符优先级
         //!=运算符先于赋值符,getchar将缓冲区内字符存入 ch 然后将ch的值与'#'对比
         putchar(ch);
     }
 }
 
+void echo_EOF() {//EOF验证
+    //键盘如何输入-1值,结束当前程序呢?
+    //系统自行模拟键盘输入时的结尾标记,Ctrl+z,Unix:Ctrl+D
+    int ch; // char型没有负值 char就是int 所以换成int无影响
+    while ((ch = getchar()) != EOF) {
+        putchar(ch);
+    }
+}
+
+void 
 int main() {
     Log();
-    echo();
+    // echo();
+    echo_EOF();
     stop();
     return 0;
 }
