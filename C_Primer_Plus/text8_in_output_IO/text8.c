5f34f3a0f9604a29a1013c049ed0a99fdd0a3f3a
JevenXing <879826973@qq.com>
2020-06-04 09:04:24 +0800

    1

--- C_Primer_Plus/text8_in_output_IO/text8.c
+++ C_Primer_Plus/text8_in_output_IO/text8.c
@@ -2,7 +2,7 @@
  * @Description    :
  * @Date           : 2020-06-02 16:39:06
  * @LastEditors    : JEVEN
- * @LastEditTime   : 2020-06-04 00:24:12
+ * @LastEditTime   : 2020-06-04 09:03:51
  * @FilePath       : \C_2020\C_Primer_Plus\text8_in_output_IO\text8.c
  */
 
@@ -26,7 +26,7 @@ void echo() { //输入验证练习
     }
 }
 
-void echo_EOF() {//EOF验证
+void echo_EOF() { // EOF验证
     //键盘如何输入-1值,结束当前程序呢?
     //系统自行模拟键盘输入时的结尾标记,Ctrl+z,Unix:Ctrl+D
     int ch; // char型没有负值 char就是int 所以换成int无影响
@@ -34,11 +34,28 @@ void echo_EOF() {//EOF验证
         putchar(ch);
     }
 }
-
+void guess() {
+    int guess = 1;
+    char response;
+    printf("从1到100选一个整数，我试着猜一下");
+    printf("it.\n如果我猜对了，请用y作答。");
+    printf("\n 如果它是错的 n。\n");
+    printf("你的数字是 %d?\n", guess);
+    while ((response = getchar()) != 'y') {
+        printf("那么它是 %d?\n", ++guess);
+        while (getchar() != '\n') {
+            //程序会相应用户的换行符操作
+            //这里是过滤首字符后面的内容
+            continue;
+        }
+    }
+    printf("我知道这个数字了 !\n");
+}
 int main() {
     Log();
-    // echo();d
-    echo_EOF();
+    // echo();
+    // echo_EOF();
+    guess();
     stop();
     return 0;
 }
