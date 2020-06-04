a0633e5d5137c2e70359b70acd600ab4a8a8d91c
JevenXing <879826973@qq.com>
2020-06-04 21:00:48 +0800

    1

--- C_Primer_Plus/text8_in_output_IO/text8.c
+++ C_Primer_Plus/text8_in_output_IO/text8.c
@@ -2,7 +2,7 @@
  * @Description    :
  * @Date           : 2020-06-02 16:39:06
  * @LastEditors    : JEVEN
- * @LastEditTime   : 2020-06-04 09:03:51
+ * @LastEditTime   : 2020-06-04 16:09:54
  * @FilePath       : \C_2020\C_Primer_Plus\text8_in_output_IO\text8.c
  */
 
@@ -34,6 +34,7 @@ void echo_EOF() { // EOF验证
         putchar(ch);
     }
 }
+//缓冲输入需过滤非有效数据
 void guess() {
     int guess = 1;
     char response;
@@ -42,20 +43,47 @@ void guess() {
     printf("\n 如果它是错的 n。\n");
     printf("你的数字是 %d?\n", guess);
     while ((response = getchar()) != 'y') {
-        printf("那么它是 %d?\n", ++guess);
+        //程序会相应用户的换行符操作
+        if (response == 'n') { //过滤非指令字符
+            printf("那么它是 %d?\n", ++guess);
+        } else {
+            printf("你输入的内容不符合要求.\n");
+        }
         while (getchar() != '\n') {
-            //程序会相应用户的换行符操作
+
             //这里是过滤首字符后面的内容
             continue;
         }
     }
     printf("我知道这个数字了 !\n");
 }
+//实现打印行列字符的函数
+void disPlay(char ch, int lines, int width) {
+    int row, col;
+    for (row = 1; row <= lines; row++) {
+        for (col = 1; col <= width; col++) {
+            putchar(ch);
+        }
+        putchar('\n'); //行结束
+    }
+}
+//混合数值和字符输入
+void showChar1() {
+    int ch;         //你要打印的字符
+    int rows, cols; //行列数
+    printf("输入字符 行数 列数,按回车结束输入,即可打印出来.\n");
+    while ((ch = getchar()) != '\n') {
+        scanf("%d %d", &rows, &cols);
+        disPlay(ch, rows, cols);
+    }
+    printf("bye");
+}
 int main() {
     Log();
     // echo();
     // echo_EOF();
-    guess();
+    // guess();
+    showChar1();
     stop();
     return 0;
 }
